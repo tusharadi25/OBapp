29f7522607e32dfd00396056cf2986fbf3953523
tusharadi25 <tusharadi25@outlook.com>
2018-06-19 10:19:10 +0530

    start screen

--- ob_explorers/lib/pages/start.dart
+++ ob_explorers/lib/pages/start.dart
@@ -17,39 +17,101 @@ class StartScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
               Expanded(
-                flex: 2,
+                flex: 3,
                 child: Container(
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      CircleAvatar(),
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 100.0,
+                        child: Image.asset('assets/logo.png'),
+                      ),
                       Padding(
                         padding: EdgeInsets.only(top: 10.0),
                       ),
-                      Text(ob.name),
+                      Text(
+                        ob.name,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24.0),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 15.0),
+                      ),
+                      Container(
+                        width: 400.0,
+                        child: Text(
+                          ob.intro,
+                          textAlign: TextAlign.center,
+                          softWrap: true,
+                          style: TextStyle(
+                              wordSpacing: 1.0,
+                              color: Colors.white70,
+                              fontWeight: FontWeight.normal,
+                              fontSize: 18.0),
+                        ),
+                      ),
                     ],
                   ),
                 ),
               ),
               Expanded(
-                flex: 2,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    RaisedButton(
-                      child: Text('LOGIN'),
-                      onPressed: () =>
-                        MyNavigator.goToLogin(context)
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 20.0),
-                    ),
-                    RaisedButton(
-                      child: Text('SIGNUP'),
-                      splashColor: Colors.red,
-                      onPressed: () => {},
-                    ),
-                  ],
+                flex: 1,
+                child: Container(
+                  width: 300.0,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      MaterialButton(
+                          elevation: 5.0,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Padding(
+                                padding: EdgeInsets.only(top: 15.0),
+                              ),
+                              Icon(
+                                Icons.person,
+                                size: 45.0,
+                              ),
+                              Text("  LOGIN",
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 24.0)),
+                            ],
+                          ),
+                          minWidth: 300.0,
+                          color: Colors.blueAccent,
+                          padding: EdgeInsets.all(10.0),
+                          splashColor: Colors.deepPurple,
+                          onPressed: () => MyNavigator.goToLogin(context)),
+                      Padding(
+                        padding: EdgeInsets.only(top: 20.0),
+                      ),
+                      MaterialButton(
+                        elevation: 5.0,
+                        padding: EdgeInsets.all(10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              Icons.person_add,
+                              size: 45.0,
+                            ),
+                            Text('  SIGNUP',
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 24.0)),
+                          ],
+                        ),
+                        minWidth: 110.0,
+                        color: Colors.blueAccent,
+                        splashColor: Colors.red,
+                        onPressed: () => {},
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
--- ob_explorers/lib/utils/ob.dart
+++ ob_explorers/lib/utils/ob.dart
@@ -3,6 +3,7 @@ class ob{
   static const String tagline = "Nature lovers and\nTrekking enthusiasts";
   static const String next = "NEXT";
   static const String gotIt = "GOT IT";
+  static const String intro ="Offbeat Explorers, is a group of nature lovers and trekking enthusiasts that schedules Outdoor events during nearly all weekends of the year.It was founded in 2015.";
 }
 
 class user{
