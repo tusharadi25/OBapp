c8914c4ba3698aaf051cab500e140390b639ebee
tusharadi25 <tusharadi25@outlook.com>
2018-06-28 00:32:13 +0530

    Loading Screen

--- ob_explorers/lib/utils/cards.dart
+++ ob_explorers/lib/utils/cards.dart
@@ -6,7 +6,6 @@ class MyHomePage extends StatelessWidget {
   const MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -17,7 +16,25 @@ class MyHomePage extends StatelessWidget {
       body: new StreamBuilder(
           stream: Firestore.instance.collection('Trips').snapshots(),
           builder: (context, snapshot) {
-            if (!snapshot.hasData) return const Text('Loading...');
+            if (!snapshot.hasData)
+              return new Container(
+                color: Colors.blue[200],
+                child: Center(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                    Image.asset(
+                      "assets/mc.png",
+                      width: 200.0,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Text("Loading.."),
+                    ),
+                    CircularProgressIndicator(),
+                  ]),
+                ),
+              );
             return new ListView.builder(
                 itemCount: snapshot.data.documents.length,
                 padding: const EdgeInsets.only(top: 10.0),
