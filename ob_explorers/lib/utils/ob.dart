0ed2a55ccb8743aef0e6aaf679d184196819066d
shree <shreedharprabhu11@gmail.com>
2018-06-29 15:55:42 +0530

    INFO full

--- ob_explorers/lib/utils/ob.dart
+++ ob_explorers/lib/utils/ob.dart
@@ -1,12 +1,12 @@
-class ob{
+class ob {
   static const String name = "Offbeat Explorers";
   static const String tagline = "Nature lovers and\nTrekking enthusiasts";
   static const String next = "NEXT";
   static const String gotIt = "GOT IT";
-  static const String intro ="Offbeat Explorers, is a group of nature lovers and trekking enthusiasts that schedules Outdoor events during nearly all weekends of the year.It was founded in 2015.";
-  static const String ab="Offbeat Explorers, is a group of nature lovers and trekking enthusiasts that schedules Outdoor events during nearly all weekends of the year.\nIt was founded in 2015. It Organizes Outdoor activities including Easy 1-day, Moderate and difficult Multi-day treks as well as Cycling, Weekend Outings, Biking-Road trips, Water-rappeling, River Rafting, Camping, Photography tours & Marathons for people of all ages-kids, youngsters, ladies & families.\nWe also take Social initiatives, Environment & conservation drives. We are Experienced in map reading and navigation.\nSo pack your bags and get ready to explore the Unexplored locations and discover Breath taking waterfalls, beautiful gorges, ancient forts and jeep trails\nhidden deep inside the jungles. Our schedules are regularly updated on our website and facebook page.\nThe basic motto behind the Establishment of Offbeat Explorers is to form a group where like-minded, fun loving and passionate souls come together in a open, volunteering and contributing environment - overcoming challenges together as a team, breaking out of ones physical and mental boundaries and coming out as friends for life and recharged human beings with a different perspective on life. We wish to store the best of the moments while travelling with us that would seize a place in your heart for the rest of your life. See you at the Trip !!!";
+  static const String intro =
+      "Offbeat Explorers, is a group of nature lovers and trekking enthusiasts that schedules Outdoor events during nearly all weekends of the year.It was founded in 2015.";
+  static const String ab =
+      "Offbeat Explorers, is a group of nature lovers and trekking enthusiasts that schedules Outdoor events during nearly all weekends of the year.\n It was founded in 2015. It Organizes Outdoor activities including Easy 1-day, Moderate and difficult Multi-day treks as well as Cycling, Weekend Outings, Biking-Road trips, Water-rappeling, River Rafting, Camping, Photography tours & Marathons for people of all ages-kids, youngsters, ladies & families.\nWe also take Social initiatives, Environment & conservation drives. We are Experienced in map reading and navigation.\nSo pack your bags and get ready to explore the Unexplored locations and discover Breath taking waterfalls, beautiful gorges, ancient forts and jeep trails\nhidden deep inside the jungles. Our schedules are regularly updated on our website and facebook page.\nThe basic motto behind the Establishment of Offbeat Explorers is to form a group where like-minded, fun loving and passionate souls come together in a open, volunteering and contributing environment - overcoming challenges together as a team, breaking out of ones physical and mental boundaries and coming out as friends for life and recharged human beings with a different perspective on life. We wish to store the best of the moments while travelling with us that would seize a place in your heart for the rest of your life. See you at the Trip !!!";
 }
 
-class user{
-
-}
+class user {}
